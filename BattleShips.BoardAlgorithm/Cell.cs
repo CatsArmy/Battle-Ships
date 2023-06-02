c7f5aa27fda6aaa8967200f8070eb8547e804513
CatsArmy <104517593+CatsArmy@users.noreply.github.com>
2023-06-02 10:06:17 +0300

    quick push to move to diffrent direcotry

---
+++ BattleShips.BoardAlgorithm/Cell.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShips.BoardAlgorithm
+{
+    public class Cell
+    {
+        BattleShip ship;
+        bool isHit;
+
+        public Cell()
+        {
+            ship = null;
+            isHit = false;
+        }
+        public BattleShip GetShip()
+        {
+            return ship;
+        }
+        public void SetShip(BattleShip _ship)
+        {
+            ship = new BattleShip(_ship/*,ship type and all that stuff from Ship class*/);
+        }
+        public bool GetIsHit()
+        {
+            return isHit;
+        }
+        public void SetIsHit(bool value)
+        {
+            isHit = value;
+        }
+    }
+}
--- BattleShips.BoardAlgorithm/Program.cs
+++ BattleShips.BoardAlgorithm/Program.cs
@@ -1,13 +1,6 @@
-﻿//https://www.c-sharpcorner.com/blogs/battleship-game-in-c-sharp-console-part-1
-//https://www.google.com/search?q=c%23+battleship+console+application&client=firefox-b-d&biw=1280&bih=587&sxsrf=APwXEdc9hSharYpAcOAdoUj_EVzp5niN6g%3A1685613381095&ei=RWt4ZNCxBf6Rxc8Piei20AM&oq=battleships+10x10+board+algorithm+C%23&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAxgBMgoIABBHENYEELADMgoIABBHENYEELADMgoIABBHENYEELADMgoIABBHENYEELADMgoIABBHENYEELADMgoIABBHENYEELADMgoIABBHENYEELADMgoIABBHENYEELADSgQIQRgAUABYAGDXNGgBcAF4AIABAIgBAJIBAJgBAMABAcgBCA&sclient=gws-wiz-serp
-
-
-using System.Diagnostics.CodeAnalysis;
-
-class Board
+﻿public class Board
 {
     Cell[,] cells;
-    
     public Board()
     {
         cells = new Cell[10, 10];
@@ -23,53 +16,8 @@ class Board
         }
     }
 
-    void PlaceShip(Ship ship)
+    void PlaceShip(BattleShip ship)
     {
 
-    }
-}
-class Cell
-{
-    Ship ship;
-    bool isHit;
-
-    public Cell()
-    {
-        ship = null;
-        isHit = false;
-    }
-    public Ship GetShip()
-    {
-        return ship;
-    }
-    public void SetShip(Ship _ship)
-    {
-        ship = new Ship(_ship/*,ship type and all that stuff from Ship class*/);
-    }
-    public bool GetIsHit()
-    {
-        return isHit;
-    }
-    public void SetIsHit(bool value)
-    {
-        isHit = value;
-    }
-}
-
-class Ship
-{
-    /*
-    "has ship type (No.)"
-    "is rotated?/direction"
-    "No. Class of ship  Size"
-    "1 	Carrier 	    5"
-    "2 	Battleship 	    4"
-    "3 	Cruiser 	    3"
-    "4 	Submarine 	    3"
-    "5 	Destroyer 	    2"
-    */
-    public Ship(Ship ship)
-    {
-        //copy porpetys
     }
 }
